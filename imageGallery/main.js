235dbe4360c40c550c81245f9a23c559a7f6fabb
shanshanluo <shanshanluo295@gmail.com>
2018-01-05 09:37:21 -0600

    add thumbBar and brightness switch effects

--- imageGallery/main.js
+++ imageGallery/main.js
@@ -6,9 +6,27 @@ var overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-  var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
-  thumbBar.appendChild(newImage);
+  for (var i=1; i<=5; i++) {
+	  var image = document.createElement('img');
+		image.setAttribute('src', 'images/pic'+i+'.jpg');
+		thumbBar.appendChild(image);
+		image.onclick = function(event) {
+		  displayedImage.setAttribute('src', event.target.getAttribute('src'));	
+		}
+	}
 
 /* Wiring up the Darken/Lighten button */
-
+  btn.onclick = function() {
+	  var brightnessCMD = btn.getAttribute('class');
+	  if(brightnessCMD === 'Darken') {
+		  // display dark style
+			overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+			btn.setAttribute('class', 'Light');
+			btn.innerHTML = 'Lighten';
+		} else {
+		  //display light style
+			overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+			btn.setAttribute('class', 'Darken');
+			btn.innerHTML = 'Darken';
+		}	
+	}
